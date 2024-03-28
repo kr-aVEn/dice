c720f1d87fb447ea9d354e95e1e904e9af320955
Tharun <thanujtharun4545@gmail.com>
2024-03-28 23:20:51 +0530

    SAMple DIce Random Selector

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function rollDice(){
-    
+    const numofdice=document.getElementById("in").value;
+    const diceimg =document.getElementById("img");
+    const result =document.getElementById("result");
+    const value=[];
+    const img=[];
+    for(i=0;i<numofdice;i++){
+        const val=Math.floor(Math.random()*6)+1;
+        value.push(val);
+        img.push(`<img src="dice/${val}.png" alt="dice ${val}" > `);
+        
+    }
+    result.textContent= 'dice:'+ value.join(',');
+    diceimg.innerHTML=img.join('');
+
 }
+    
